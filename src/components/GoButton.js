7e005a73cc886fb094c599d90a31c1494f3908d3
Tom Canham <alphasimian@gmail.com>
2018-12-22 21:53:15 -0800

    Begin importing CSS via webpack

--- src/components/GoButton.js
+++ src/components/GoButton.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './GoButton.css'
 
-const style = {
-  flexBasis: '50%',
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-around',
-  width: '99%',
-  margin: '10px',
-  height: '50px',
-  backgroundColor: 'yellow',
-  fontSize: '1.5rem',
-  borderRadius: '10px'
-}
+// const style = {
+//   flexBasis: '50%',
+//   display: 'flex',
+//   flexDirection: 'row',
+//   justifyContent: 'space-around',
+//   width: '99%',
+//   margin: '10px',
+//   height: '50px',
+//   backgroundColor: 'yellow',
+//   fontSize: '1.5rem',
+//   borderRadius: '10px'
+// }
 
-const GoButton = (props) => {
-  const { onClick } = props
-
-  return <button style={style} onClick={onClick}>Go!</button>
+const GoButton = ({ onClick }) => {
+  return <button className="go-button" onClick={onClick}>Go!</button>
 }
 
 GoButton.propTypes = {
